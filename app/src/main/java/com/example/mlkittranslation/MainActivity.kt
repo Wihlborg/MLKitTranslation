318dea8248d917af4d7aee1fa99e48358664135b
Henrik Vihlborg <henrik.vihlborg0017@stud.hkr.se>
2020-02-01 00:24:52 +0100

    Added camera, button, and FireBase.

--- app/src/main/java/com/example/mlkittranslation/MainActivity.kt
+++ app/src/main/java/com/example/mlkittranslation/MainActivity.kt
@@ -1,12 +1,153 @@
 package com.example.mlkittranslation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import java.util.concurrent.Executors
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        if (allPermissionsGranted()){
+            viewFinder.post { startCamera() }
+        } else{
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    private val analyzer = MyImageAnalyzer()
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    private fun startCamera(){
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(720,1280))
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+
+            updateTransform()
+        }
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
+        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
+            imageCapture.takePicture(executor, object : ImageCapture.OnImageCapturedListener() {
+                override fun onCaptureSuccess(image: ImageProxy?, rotationDegrees: Int) {
+                    analyzer.analyze(image, rotationDegrees)
+                    image?.close()
+                }
+            })
+        }
+
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    private fun updateTransform(){
+        val matrix = Matrix()
+
+        val centerX = viewFinder.width/ 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when(viewFinder.display.rotation){
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        viewFinder.setTransform(matrix)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+            if (requestCode == REQUEST_CODE_PERMISSIONS){
+                if (allPermissionsGranted()){
+                    viewFinder.post{startCamera()}
+                } else{
+                    Toast.makeText(this, "Permissions not granted by user", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
+
+
+private class MyImageAnalyzer: ImageAnalysis.Analyzer {
+
+    private fun degreesToFirebaseRotation(degrees: Int): Int = when(degrees){
+        0 -> FirebaseVisionImageMetadata.ROTATION_0
+        90 -> FirebaseVisionImageMetadata.ROTATION_90
+        180 -> FirebaseVisionImageMetadata.ROTATION_180
+        270 -> FirebaseVisionImageMetadata.ROTATION_270
+        else -> throw Exception("Rotation must be 0, 90, 180, or 270.")
+
+    }
+
+    override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+        val mediaImage = image?.image
+        val imageRotation = degreesToFirebaseRotation(rotationDegrees)
+
+        if (mediaImage != null){
+            val image = FirebaseVisionImage.fromMediaImage(mediaImage, imageRotation)
+
+            val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
+            detector.processImage(image)
+                .addOnSuccessListener{firebaseVisionText ->
+                    println(firebaseVisionText.text)} //TODO Data binding
+                .addOnFailureListener { exception -> Log.e("Exception", "${exception.printStackTrace()}") }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/mlkittranslation/TextData.kt
@@ -0,0 +1,7 @@
+package com.example.mlkittranslation
+
+import androidx.databinding.ObservableField
+
+data class TextData(val text: ObservableField<String>)
+
+//TODO implement data binding
